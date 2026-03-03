00b51961d89f07b67c4449032b4a4132e7c78594
nradair <crixusdysprosium@gmail.com>
2026-03-03 07:58:56 -0500

    minmax hyperparameter update

--- MinMax.py
+++ MinMax.py
@@ -22,36 +22,7 @@ warnings.filterwarnings('ignore')
 
 RANDOM_SEED = 68
 
-# if __name__ == '__main__':
-#     data_dir = 'data_readinglevel'
-#     x_train_df = pd.read_csv(os.path.join(data_dir, 'x_train.csv'))
-#     y_train_df = pd.read_csv(os.path.join(data_dir, 'y_train.csv'))
-
-#     N, n_cols = x_train_df.shape
-#     print("Shape of x_train_df: (%d, %d)" % (N, n_cols))
-#     print("Shape of y_train_df: %s" % str(y_train_df.shape))
-
-#     # Print out 8 random entries
-#     tr_text_list = x_train_df['text'].values.tolist()
-#     prng = np.random.RandomState(101)
-#     rows = prng.permutation(np.arange(y_train_df.shape[0]))
-#     for row_id in rows[:8]:
-#         text = tr_text_list[row_id]
-#         print("row %5d | %s BY %s | y = %s" % (
-#             row_id,
-#             y_train_df['title'].values[row_id],
-#             y_train_df['author'].values[row_id],
-#             y_train_df['Coarse Label'].values[row_id],
-#             ))
-
-#         line_list = textwrap.wrap(tr_text_list[row_id],
-#             width=70,
-#             initial_indent='  ',
-#             subsequent_indent='  ')
-#         print('\n'.join(line_list))
-#         print("")
-
-def make_mlp_pipeline(layers, activation, solver, alpha, batch_size, learning_rate):
+def make_mlp_pipeline(layers, activation, solver, alpha, batch_size, learning_rate, learning_rate_init):
     pipeline = sklearn.pipeline.Pipeline(
         steps= [
             ('rescaler', sklearn.preprocessing.MinMaxScaler()),
@@ -60,7 +31,8 @@ def make_mlp_pipeline(layers, activation, solver, alpha, batch_size, learning_ra
                                                            solver=solver,
                                                            alpha=alpha,
                                                            batch_size=batch_size,
-                                                           learning_rate=learning_rate
+                                                           learning_rate=learning_rate,
+                                                           learning_rate_init=learning_rate_init
             ))
         ]
     )
@@ -92,7 +64,7 @@ def hyperparameter_selection(x_dev, x_train_df, y_train_df):
     y_labels = y_train_df['Coarse Label'].tolist()
     y_dev = np.array(y_labels)
 
-    max_auc, best_c, best_bs, best_layer = 0, 0, 0, []
+    max_auc, best_c, best_bs, best_layer, best_lr, best_lr_init = 0, 0, 0, [], 0, 0
     best_per_layer = {}
     cat = x_train_df["author"].values
     # Optimize C with cross validation 
@@ -100,38 +72,48 @@ def hyperparameter_selection(x_dev, x_train_df, y_train_df):
     # for layer in ([2, 2], [4, 4], [8, 8], [16, 16], [32, 32], [64, 64], [128, 128]):
     for layer in ([16, 16], [32, 32]):
         for bs in [32, 64, 128]:
-            for c in np.logspace(-4, 4, 17)[0:-5]:
-                auc_sum = 0
-                pipe = make_mlp_pipeline(layers=layer, activation='relu', solver='adam', alpha=c, batch_size=bs, learning_rate='invscaling')
-                for train_ind, val_ind in kf.split(x_dev, y_dev, cat):
-                    pipe.fit(x_dev[train_ind], y_dev[train_ind])
-                    y_hat = pipe.predict_proba(x_dev[val_ind])[:, 1]
-                    auc = sklearn.metrics.roc_auc_score(y_dev[val_ind], y_hat)
-                    auc_sum += auc
-                avg_auc = auc_sum / 10
+            for lr in ["constant", "adaptive", "invscaling"]:
+                for lr_init in np.linspace(1e-5, 1e-3, 5):
+                    for c in np.logspace(-4, 4, 17)[0:-5]:
+                        auc_sum = 0
+                        pipe = make_mlp_pipeline(layers=layer,
+                                                activation='relu',
+                                                solver='adam',
+                                                alpha=c,
+                                                batch_size=bs,
+                                                learning_rate=lr,
+                                                learning_rate_init=lr_init)
+                        for train_ind, val_ind in kf.split(x_dev, y_dev, cat):
+                            pipe.fit(x_dev[train_ind], y_dev[train_ind])
+                            y_hat = pipe.predict_proba(x_dev[val_ind])[:, 1]
+                            auc = sklearn.metrics.roc_auc_score(y_dev[val_ind], y_hat)
+                            auc_sum += auc
+                        avg_auc = auc_sum / 10
 
-                print(f"AUC {avg_auc:.6f} with layers {layer}, batch size {bs}, c {c:e}")
+                        print(f"AUC {avg_auc:.6f} with layers {layer}, batch size {bs}, learning rate {lr}, initial lr {lr_init}, c {c:e}")
 
-                # Prioritize a lower c value
-                if avg_auc > max_auc:
-                    best_c, max_auc, best_bs, best_layer = c, avg_auc, bs, layer
-                if str(layer) in best_per_layer.keys():
-                    if avg_auc > best_per_layer[str(layer)][0]:
-                        best_per_layer[str(layer)] = [max_auc, best_c.item(), best_bs]
-                else:
-                    best_per_layer[str(layer)] = [max_auc, best_c.item(), best_bs]
+                        # Prioritize a lower c value
+                        if avg_auc > max_auc:
+                            best_c, max_auc, best_bs, best_layer = c, avg_auc, bs, layer
+                        if str(layer) in best_per_layer.keys():
+                            if avg_auc > best_per_layer[str(layer)][0]:
+                                best_per_layer[str(layer)] = [max_auc, best_c.item(), best_bs]
+                        else:
+                            best_per_layer[str(layer)] = [max_auc, best_c.item(), best_bs]
 
     print("-"*64)
     print("Best AUC:", max_auc)
     print("Best C:", best_c)
     print("Best hidden layers:", best_layer)
     print("Best batch size", best_bs)
+    print("Best learning rate", best_lr)
+    print("Best initial lr", best_lr_init)
     print("-"*64)
     print(best_per_layer)
 
-    return best_c, best_bs, best_layer, best_per_layer
+    return best_c, best_bs, best_layer, best_lr, best_lr_init, best_per_layer
 
-def test_prediction(x_dev, y_train_df, x_test, c, bs, layer):
+def test_prediction(x_dev, y_train_df, x_test, c, bs, layer, lr, lr_init):
     y_labels = y_train_df['Coarse Label'].tolist()
     y_dev = np.array(y_labels)
     pipe = make_mlp_pipeline(layers=layer, activation='relu', solver='adam', alpha=c, batch_size=bs, learning_rate='invscaling')
@@ -158,8 +140,8 @@ def main():
        'info_type_token_ratio', 'info_characters', 'info_syllables',
        'info_words', 'info_wordtypes']
     x_dev, x_train_df, y_train_df, x_test = load_data(metrics)
-    c, bs, layer, best_per_layer = hyperparameter_selection(x_dev, x_train_df, y_train_df)
-    test_prediction(x_dev, y_train_df, x_test, c, bs, layer)
+    c, bs, layer, lr, lr_init, best_per_layer = hyperparameter_selection(x_dev, x_train_df, y_train_df)
+    test_prediction(x_dev, y_train_df, x_test, c, bs, layer, lr, lr_init)
 
 if __name__ == "__main__":
     main()
